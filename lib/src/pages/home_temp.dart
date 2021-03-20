a175c4223bd5c726bff597269451e6164e298320
Oscar <Osramirezf@gmail.com>
2021-03-20 16:19:38 -0300

    Curso de udemy - Flutter Fernando Herrera

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes temp'),
+      ),
+      body: ListView(children: _crearItemsCorta()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+    for (String op in opciones) {
+      final tempWidget = ListTile(
+        title: Text(op),
+      );
+      lista
+        ..add(tempWidget)
+        ..add(Divider(
+          color: Colors.black87,
+        ));
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.adb),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
+}
